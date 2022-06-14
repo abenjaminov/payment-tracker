bca08771c5430ceeed3f8031ac074e917db62186
Asaf Benjaminov <abenjaminov@gmail.com>
2022-06-14 14:33:52 +0300

    Add receipts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -9,7 +9,7 @@ import {AirTableEntity, GetMonthlyRevenueArgs} from "../models";
 import {ApiService} from "./api.service";
 import {CacheUrlGroup} from "./cache";
 
-export const apiVersion: string = '1.1.0';
+export const apiVersion: string = '1.1.2';
 
 enum CacheUrlGroupKey {
   sessionsSaved
