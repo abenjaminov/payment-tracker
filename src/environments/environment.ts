7fb42804135ce915dc51814efe3a6fe1322be795
Asaf Benjaminov <abenjaminov@gmail.com>
2024-03-14 17:04:33 +0200

    change api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
    airtableBaseId: 'apphIpoln707ntQT5',
-   airtableApiKey: 'keyi6s9aDnC2cnIsb',
+   airtableApiKey: 'patnu4Id9eAdfMPrj.3f9fa820ded0b97bebe8660340b0e78d0a40b6e02986e3da8280766365afa004',
    production: false
 };
